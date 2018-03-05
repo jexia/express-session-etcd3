56c6dd17de51bfb1c610700d2864fcc79910a6b2
William Grasel <d.rufos@gmail.com>
2018-03-05 15:18:13 -0300

    build: fixing build formats

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,7 +12,7 @@ export default {
   input: `src/${libraryName}.ts`,
   output: [
     { file: pkg.main, name: camelCase(libraryName), format: 'cjs' },
-    { file: pkg.module, format: 'cjs' },
+    { file: pkg.module, format: 'es' },
   ],
   sourcemap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
