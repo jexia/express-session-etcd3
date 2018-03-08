2055d314e0365eb93553dbae0eb010dc46c43f7c
William Grasel <d.rufos@gmail.com>
2018-03-08 10:39:04 -0300

    docs: adding base docs

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # express-session-etcd3
 
-This is a etcd v3 session store for Express.
+An ETCD v3 store adapter for [Express session](https://github.com/expressjs/session) using [etcd3](https://github.com/mixer/etcd3) client.
 
 ## Setup
 
+Install this package with all necessary peer dependencies:
+
 ```sh
-npm install express-session-etcd3 express-session
+npm install express-session-etcd3 express-session etcd3
 ```
 
-Pass the `express-session` store into `express-session-etcd3` to create a `Etcd3Store` constructor.
+Pass the necessary configuration to access your etcd v3 base into the `Etcd3Store` constructor as `options`:
 
 ```js
 var session = require('express-session');
@@ -21,6 +23,30 @@ app.use(session({
 }));
 ```
 
+### Options
+
+You can find all the [available options](https://willgm.github.io/express-session-etcd3/interfaces/etcd3storeoptions.html) at the documentation.
+
+## Documentation
+
+Our [TypeDoc docs are available here](https://willgm.github.io/express-session-etcd3/).
+
+Our [test cases](https://github.com/willgm/express-session-etcd3/tree/master/test) are also quite readable.
+
+## Contributing
+
+Running tests for this module requires running an etcd3 server locally. The tests try to use the default port initially, and you can configure this by setting the ETCD_ADDR environment variable, like export ETCD_ADDR=localhost:12345.
+
+There is a Docker image ready to use, which can be easily build with:
+
+```sh
+npm run docker
+```
+
+## Credits
+
+This project was easily bootstrapped with [TypeScript library starter](https://github.com/alexjoverm/typescript-library-starter), thanks for sharing it!
+
 ## License
 
-MIT
+[MIT](https://github.com/willgm/express-session-etcd3/blob/master/LICENSE)
--- src/express-session-etcd3.ts
+++ src/express-session-etcd3.ts
@@ -1,17 +1,45 @@
 import * as session from 'express-session'
 import { Etcd3, IOptions } from 'etcd3'
 
+/**
+ * One day in seconds.
+ */
 export const oneDay = 86400
 
+/**
+ * Configuration options for the etcd v3
+ */
 export interface Etcd3StoreOptions extends IOptions {
+  /**
+   * Prefix used to record the keys of all the sessions
+   *
+   * Defaults to `sess`.
+   */
   prefix?: string
 }
 
+/**
+ * Default configuration values for the etcd v3 options
+ */
 export const defaultOptions: Etcd3StoreOptions = Object.freeze({
   prefix: 'sess',
   hosts: '127.0.0.1:2379'
 })
 
+/**
+ * An etcd v3 store adapter for Express session using [etcd3](https://github.com/mixer/etcd3) client.
+ *
+ * ```
+ * var session = require('express-session');
+ * var Etcd3Store = require('express-session-etcd3');
+ *
+ * app.use(session({
+ *     store: new Etcd3Store(options),
+ *     secret: 'keyboard cat',
+ *     resave: false
+ * }));
+ * ```
+ */
 export default class Etcd3Store extends session.Store {
   constructor(
     private config: Etcd3StoreOptions = defaultOptions,
@@ -20,6 +48,14 @@ export default class Etcd3Store extends session.Store {
     super(config)
   }
 
+  /**
+   * This method is used to get a session from the store given a session
+   * ID (`sid`). The `callback` should be called as `callback(error, session)`.
+   *
+   * The `session` argument should be a session if found, otherwise `null` or
+   * `undefined` if the session was not found (and there was no error). A special
+   * case is made when `error.code === 'ENOENT'` to act like `callback(null, null)`.
+   */
   get = (sid: string, callback: (err: any, session: Express.SessionData) => void): void => {
     try {
       this.client
@@ -31,6 +67,11 @@ export default class Etcd3Store extends session.Store {
     }
   }
 
+  /**
+   * This required method is used to upsert a session into the store given a
+   * session ID (`sid`) and session (`session`) object. The callback should be
+   * called as `callback(error)` once the session has been set in the store.
+   */
   set = (sid: string, session: Express.SessionData, callback: (err: any) => void): void => {
     try {
       const lease = this.client
@@ -43,10 +84,23 @@ export default class Etcd3Store extends session.Store {
     }
   }
 
+  /**
+   * This method is used to "touch" a given session given a
+   * session ID (`sid`) and session (`session`) object. The `callback` should be
+   * called as `callback(error)` once the session has been touched.
+   *
+   * This is primarily used when the store will automatically delete idle sessions
+   * and this method is used to signal to the store the given session is active,
+   * potentially resetting the idle timer.
+   */
   touch = (sid: string, session: Express.SessionData, callback: (err: any) => void): void => {
     this.set(sid, session, callback)
   }
 
+  /**
+   * This method is used to get all sessions in the store as an array. The
+   * `callback` should be called as `callback(error, sessions)`.
+   */
   all = (callback: (err: any, obj: { [sid: string]: Express.SessionData }) => void): void => {
     try {
       this.client
@@ -60,6 +114,10 @@ export default class Etcd3Store extends session.Store {
     }
   }
 
+  /**
+   * This method is used to get the count of all sessions in the store.
+   * The `callback` should be called as `callback(error, len)`.
+   */
   length = (callback: (err: any, length: number) => void): void => {
     try {
       this.client
@@ -72,6 +130,11 @@ export default class Etcd3Store extends session.Store {
     }
   }
 
+  /**
+   * This method is used to destroy/delete a session from the store given
+   * a session ID (`sid`). The `callback` should be called as `callback(error)`
+   * once the session is destroyed.
+   */
   destroy = (sid: string, callback: (err: any) => void): void => {
     try {
       this.client
@@ -83,6 +146,10 @@ export default class Etcd3Store extends session.Store {
     }
   }
 
+  /**
+   * This method is used to delete all sessions from the store. The `callback`
+   * should be called as `callback(error)` once the store is cleared.
+   */
   clear = (callback: (err: any) => void): void => {
     try {
       this.client
@@ -94,10 +161,16 @@ export default class Etcd3Store extends session.Store {
     }
   }
 
+  /**
+   * Build the etcd key with the right prefix and the givin session ID (`sid`)
+   */
   private key(sid = ''): string {
     return (this.config.prefix || defaultOptions.prefix) + '/' + sid
   }
 
+  /**
+   * Get the Time to Live (`ttl`) of the session
+   */
   private getTTL(sess: Express.SessionData, sid: string): number {
     const storeTtl: any = (this as any)['ttl']
     if (typeof storeTtl === 'number') return storeTtl
